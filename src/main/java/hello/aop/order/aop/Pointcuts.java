b999721b0ee513b7c7af63bff34ce4c2d82284ed
ljs3536 <wlstjr320@naver.com>
2026-03-04 20:04:49 +0900

    스프링 AOP 구현5 - 어드바이스 순서

--- src/main/java/hello/aop/order/aop/Pointcuts.java
+++ src/main/java/hello/aop/order/aop/Pointcuts.java
@@ -4,14 +4,12 @@ import org.aspectj.lang.annotation.Pointcut;
 
 public class Pointcuts {
 
-    //hello.aop.order 패키지와 하위 패키지
+    //hello.springaop.app 패키지와 하위 패키지
     @Pointcut("execution(* hello.aop.order..*(..))")
-    private void allOrder(){} //pointcut signature
-
-    //클래스 이름 패턴이 *Service
+    public void allOrder(){}
+    //타입 패턴이 *Service
     @Pointcut("execution(* *..*Service.*(..))")
     public void allService(){}
-
     //allOrder && allService
     @Pointcut("allOrder() && allService()")
     public void orderAndService(){}
